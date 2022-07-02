467b064c680166bc63627d84eb76003ea1cd1968
smilehjl <1694621989@qq.com>
2022-07-02 09:59:57 +0800

    third commit

--- src/main/java/maven_test.java
+++ src/main/java/maven_test.java
@@ -6,5 +6,6 @@ public class maven_test {
     public static void main(String [] args){
         System.out.println("欢迎使用maven");
         System.out.println("java 很好！");
+        System.out.println("这里是hot-fix分支");
     }
 }
